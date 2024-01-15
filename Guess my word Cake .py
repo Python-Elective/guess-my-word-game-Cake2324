2c414b9bb57287711060b65a57517e992753c6f4
Cake2324 <supichaya.ma2025@kmids.ac.th>
2024-01-15 09:15:42 +0700

    add commmit

--- Guess my word Cake .py
+++ Guess my word Cake .py
@@ -1,1 +1,21 @@
-ki90po0-pow
+# improt string
+# alphabet = string.ascii_lowercase
+
+# for lettter in letters_quessed:
+#     alphabet = alphabet.replace(letter,'')
+
+# return alphabet
+
+alphabet = list(string.ascii_lowercase)
+for letterr in alphabet:
+    if letter in letter_quesssed:
+        alphabet.remove(letter)
+
+return ''.join(alphabet)
+                for lettter in alphabet
+                       if letter not in letters_quessed
+
+# Opion4
+# output_string = ''
+# for everyletter in alphabet
+
